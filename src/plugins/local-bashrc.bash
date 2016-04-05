35f1f8cba576bb8c11cb9eae7dfc87e95aaeb890
Eric Villard <dev@eviweb.fr>
2016-04-05 13:53:09 +0200

    fix local bashrc includes
    forget to use the $BASHRC_INC environment variable
    misuse of the $BASHRC_INC environment variable
    
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- src/plugins/local-bashrc.bash
+++ src/plugins/local-bashrc.bash
@@ -1,10 +1,10 @@
 #! /bin/bash
 # local initialization scripts
-if [[ -d $HOME/.bashrc.d ]]; then
-    if [ -z "${BASHRC_INC}" ]; then
-        BASHRC_INC=$HOME/.bashrc.d
-    fi
-    for i in $HOME/.bashrc.d/*.*sh; do
+if [ -z "${BASHRC_INC}" ]; then
+    BASHRC_INC=$HOME/.bashrc.d
+fi
+if [[ -d ${BASHRC_INC} ]]; then    
+    for i in ${BASHRC_INC}/*.*sh; do
         if [ -r $i ]; then
             . $i
         fi
