cff820ad67b4f5c4c533ad6f766271e4272f7740
Eric Villard <dev@eviweb.fr>
2016-07-29 16:11:39 +0200

    override bash-it native safe_append_prompt_command
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- src/lib/00-init.bash
+++ src/lib/00-init.bash
@@ -1,3 +1,15 @@
+safe_append_prompt_command()
+{
+    if [[ -n $1 ]] && [[ -n $PROMPT_COMMAND ]]; then
+        case $PROMPT_COMMAND in
+            $1) ;;
+            *) PROMPT_COMMAND="$1;$PROMPT_COMMAND";;
+        esac
+    elif [[ -n $1 ]]; then
+        PROMPT_COMMAND="$1"
+    fi
+}
+
 disable_sdkman_gvm_alias()
 {
     local config="${HOME}/.sdkman/etc/config"
