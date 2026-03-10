3e51e02584f6990ec35f8673df9757328321882c
Eric Villard <dev@eviweb.fr>
2026-03-10 10:40:22 +0100

    add gax git command to add file except the ones specified
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- src/custom/git.bash
+++ src/custom/git.bash
@@ -4,3 +4,12 @@ git_add_modified()
 {
     ga $(gs | grep -Pe "modified:.*" | grep -Poe "(?<=modified:   )([^ ]+)" | tr "\n" " ")
 }
+
+gax()
+{
+    if [ $# -eq 0 ]; then
+        git add .
+    else
+        git add -- . $(printf ":!%s " "$@")
+    fi
+}
